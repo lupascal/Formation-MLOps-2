b6054b0b28d8ecf1b753f9c0b66a468df77ce392
lupascal <mr.lu.pascal@gmail.com>
2025-10-03 12:43:19 +0000

    .

--- exposition/model_as_a_service/app.py
+++ exposition/model_as_a_service/app.py
@@ -10,9 +10,13 @@ default_wind_speed_avg = 0
 received_wind_speed_avg = st.text_input(
     "Wind speed average (Ws1_avg)", default_wind_speed_avg)
 
+default_wind_speed_avg2 = 0
+received_wind_speed_avg2 = st.text_input(
+    "Wind speed average (Ws2_avg)", default_wind_speed_avg2)
+
 if st.button('Predict with an embedded model !'):
     prediction = requests.get(
-        f'http://{INFERENCE_HOST}:5000/predict?Ws1_avg={received_wind_speed_avg}').json()
+        f'http://{INFERENCE_HOST}:5000/predict?Ws1_avg={received_wind_speed_avg}&Ws2_avg={received_wind_speed_avg2}').json()
     st.write('👇 Prediction with wind speed average at 👇' +
              received_wind_speed_avg)
     st.write(prediction)
